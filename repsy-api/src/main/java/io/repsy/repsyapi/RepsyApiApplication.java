0b3c3e097b4c32a1cdb48c70ec2e4f99a10133fe
Mehmet Genc <mehmetgencv@gmail.com>
2025-04-22 19:28:55 +0300

    init for repsy api app

---
+++ repsy-api/src/main/java/io/repsy/repsyapi/RepsyApiApplication.java
@@ -0,0 +1,13 @@
+package io.repsy.repsyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepsyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepsyApiApplication.class, args);
+	}
+
+}
